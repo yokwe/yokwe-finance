3b6ac88fff98e75795617dbc2cbe829dff94cf35
yokwe <hasegawa.yasuhiro@gmail.com>
2026-05-07 15:32:03 +0900

    Use StorageRakuten.TradingStockUS

--- src/main/java/yokwe/finance/report/stock/us/UpdateReport.java
+++ src/main/java/yokwe/finance/report/stock/us/UpdateReport.java
@@ -5,6 +5,7 @@ import java.time.LocalDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import yokwe.finance.report.stats.StockStats;
 import yokwe.util.FileUtil;
@@ -18,23 +19,24 @@ import yokwe.util.update.UpdateBase;
 
 public class UpdateReport extends UpdateBase {
 	private static final org.slf4j.Logger logger = yokwe.util.LoggerUtil.getLogger();
-	
+
 	public static Makefile MAKEFILE = Makefile.builder().
 			input(
 					yokwe.finance.data.stock.us.StorageUS.StockInfo,
 					yokwe.finance.data.stock.us.StorageUS.StockPriceOHLCV,
-					yokwe.finance.data.stock.us.StorageUS.StockDiv
+					yokwe.finance.data.stock.us.StorageUS.StockDiv,
+					yokwe.finance.data.provider.rakuten.StorageRakuten.TradingStockUS
 				).
 			output(StorageUS.Report).
 			build();
-	
+
 	public static void main(String[] args) {
 		callUpdate();
 	}
-	
+
 	@Override
 	public void update() {
-		var list = getReportList();		
+		var list = getReportList();
 		generateReport(list);
 		// save
 		// generateReport saves file
@@ -42,50 +44,52 @@ public class UpdateReport extends UpdateBase {
 	private List<ReportForm> getReportList() {
 		var dateStop  = MarketHoliday.JP.getLastTradingDate();
 		logger.info("dateStop  {}", dateStop);
-		
+
+		var rakutenSet = yokwe.finance.data.provider.rakuten.StorageRakuten.TradingStockUS.getList().stream().map(o -> o.stockCode).collect(Collectors.toSet());
+
 		var list = new ArrayList<ReportForm>();
 		{
 			for(var stockInfo: yokwe.finance.data.stock.us.StorageUS.StockInfo.getList()) {
 				var stockCode = stockInfo.stockCode;
 				var priceList = yokwe.finance.data.stock.us.StorageUS.StockPriceOHLCV.getList(stockCode);
 				var divList   = yokwe.finance.data.stock.us.StorageUS.StockDiv.getList(stockCode);
-				
+
 				if (priceList.size() < 10) {
 					logger.info("skip  {}  {}  {}", priceList.size(), stockCode, stockInfo.name);
 					continue;
 				}
-				
+
 				ReportForm report = new ReportForm();
 				report.stockCode = stockCode;
 				report.type      = stockInfo.type.simpleType.toString();
 				report.sector    = stockInfo.sector;
 				report.industry  = stockInfo.industry;
-				
+
 				report.divc     = -1;
 				// set sector and industry
-				
+
 				report.name      = stockInfo.name;
-				
+
 				{
 					StockStats stockStats = StockStats.getInstance(stockCode, dateStop, priceList, divList);
-					
+
 					report.price     = stockStats.price;
 					report.pricec    = priceList.size();
 					report.last      = stockStats.last;
-					
+
 					report.rorNoReinvested = stockStats.rorNoReinvested;
-					
+
 					report.sd        = stockStats.sd;
 					report.hv        = stockStats.hv;
 					report.rsi       = stockStats.rsi14;
 //					report.rsi14     = stockStats.rsi14;
 //					report.rsi7      = stockStats.rsi7;
-					
+
 					report.min       = stockStats.min;
 					report.max       = stockStats.max;
 					report.minY3     = stockStats.minY3;
 					report.maxY3     = stockStats.maxY3;
-					
+
 //					if (stats.divc == -1) {
 //						stats.divc          = stockStats.divc;
 //					}
@@ -102,11 +106,12 @@ public class UpdateReport extends UpdateBase {
 					report.vol5      = (long)(stockStats.vol5  * report.price);
 					report.vol21     = (long)(stockStats.vol21 * report.price);
 				}
-				
+
+				// FXIME
 				report.nisa    = "";
-				report.rakuten = "";
+				report.rakuten = rakutenSet.contains(stockCode) ? "1" : "";
 				report.nikko   = "";
-								
+
 				list.add(report);
 			}
 		}
@@ -120,21 +125,21 @@ public class UpdateReport extends UpdateBase {
 		try {
 			// start LibreOffice process
 			LibreOffice.initialize();
-			
+
 			SpreadSheet docLoad = new SpreadSheet(URL_TEMPLATE, true);
 			SpreadSheet docSave = new SpreadSheet();
-			
+
 			String sheetName = Sheet.getSheetName(ReportForm.class);
 			logger.info("sheet     {}", sheetName);
 			docSave.importSheet(docLoad, sheetName, docSave.getSheetCount());
 			Sheet.fillSheet(docSave, reportList);
-			
+
 			// remove first sheet
 			docSave.removeSheet(docSave.getSheetName(0));
 
 			docSave.store(urlReport);
 			logger.info("output    {}", urlReport);
-			
+
 			docLoad.close();
 			logger.info("close     docLoad");
 			docSave.close();
@@ -147,9 +152,9 @@ public class UpdateReport extends UpdateBase {
 			var timestamp  = DateTimeFormatter.ofPattern("yyyyMMdd-HHmmss").format(LocalDateTime.now());
 			var newName = "report-" + timestamp + ".ods";
 			var destFile = StorageUS.Report.getFile(newName);
-			
+
 			logger.info("copy {} to {}", StorageUS.Report.getFile(), destFile);
-			
+
 			FileUtil.copy(StorageUS.Report.getFile(), destFile);
 		}
 	}
