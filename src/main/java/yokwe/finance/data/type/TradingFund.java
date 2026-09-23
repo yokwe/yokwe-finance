eaa02de4ad251aab5076541189369b0b4f385b1f
yokwe <hasegawa.yasuhiro@gmail.com>
2026-09-23 21:16:41 +0900

    Make simpler

--- src/main/java/yokwe/finance/data/type/TradingFund.java
+++ src/main/java/yokwe/finance/data/type/TradingFund.java
@@ -1,35 +1,9 @@
 package yokwe.finance.data.type;
 
 import java.math.BigDecimal;
-import java.util.HashSet;
-import java.util.Set;
-
-import yokwe.finance.data.provider.click.StorageClick;
-import yokwe.finance.data.provider.nikko.StorageNikko;
-import yokwe.finance.data.provider.rakuten.StorageRakuten;
-import yokwe.finance.data.provider.smtb.StorageSMTB;
-import yokwe.finance.data.provider.sony.StorageSony;
 
 public class TradingFund implements Comparable<TradingFund> {
 	public static final BigDecimal SALES_FEE_UNKNOWN = BigDecimal.valueOf(-1);
-
-
-	private static Set<String> isinSet = isinCodeSet();
-	private static Set<String> isinCodeSet() {
-		var set = new HashSet<String>();
-
-		set.addAll(StorageClick.TradingFundJPClick.getList().stream().map(o -> o.isinCode).toList());
-		set.addAll(StorageNikko.TradingFundJPNikko.getList().stream().map(o -> o.isinCode).toList());
-		set.addAll(StorageRakuten.TradingFundJPRakuten.getList().stream().map(o -> o.isinCode).toList());
-		set.addAll(StorageSMTB.TradingFundJPSMTB.getList().stream().map(o -> o.isinCode).toList());
-		set.addAll(StorageSony.TradingFundJPSony.getList().stream().map(o -> o.isinCode).toList());
-
-		return set;
-	}
-	public static boolean isTradingFund(String isinCode) {
-		return isinSet.contains(isinCode);
-	}
-
 
 	public String     isinCode;
 	public BigDecimal salesFee;  // 0 for no load
