c6e7100acf18deedeed573360ba4c6e5adfd87f2
yokwe <hasegawa.yasuhiro@gmail.com>
2026-09-23 21:18:05 +0900

    Add divDate, divValue, divPrice and divYield

--- src/main/java/yokwe/finance/data/type/FundDivScore.java
+++ src/main/java/yokwe/finance/data/type/FundDivScore.java
@@ -1,42 +1,64 @@
 package yokwe.finance.data.type;
 
 import java.math.BigDecimal;
+import java.time.LocalDate;
 
 import yokwe.util.ToString;
 
 public class FundDivScore implements Comparable<FundDivScore> {
-	public static BigDecimal NO_VALUE = BigDecimal.ONE.negate();
-	
+	public static final BigDecimal NO_VALUE = BigDecimal.ONE.negate();
+
 	public static boolean isValid(BigDecimal value) {
 		return value.compareTo(NO_VALUE) != 0;
 	}
-	
+
+	public static final LocalDate NO_DATE = LocalDate.of(2099, 1, 1);
+
+	public static boolean isValid(LocalDate value) {
+		return value.compareTo(NO_DATE) != 0;
+	}
+
+
 	public String     isinCode;
+	public String     fundCode;
+	public String     stockCode;
 	public BigDecimal score1Y;
 	public BigDecimal score3Y;
 	public BigDecimal score5Y;
 	public BigDecimal score10Y;
-	
-	public FundDivScore(String isinCode, BigDecimal score1Y, BigDecimal socre3Y, BigDecimal socre5Y, BigDecimal score10Y) {
-		this.isinCode = isinCode;
+	public LocalDate  divDate;
+	public BigDecimal divValue;
+	public BigDecimal divPrice;
+	public BigDecimal divYield;
+	public String     name;
+
+	public FundDivScore(
+		String isinCode, String fundCode, String stockCode,
+		BigDecimal score1Y, BigDecimal socre3Y, BigDecimal socre5Y, BigDecimal score10Y,
+		LocalDate divDate, BigDecimal divValue, BigDecimal divPrice, BigDecimal divYield,
+		String name) {
+		this.isinCode  = isinCode;
+		this.fundCode  = fundCode;
+		this.stockCode = stockCode;
+
 		this.score1Y  = score1Y;
 		this.score3Y  = socre3Y;
 		this.score5Y  = socre5Y;
 		this.score10Y = score10Y;
+
+		this.divDate  = divDate;
+		this.divValue = divValue;
+		this.divPrice = divPrice;
+		this.divYield = divYield;
+
+		this.name      = name;
 	}
-	public FundDivScore(String isinCode) {
-		this.isinCode = isinCode;
-		this.score1Y  = null;
-		this.score3Y  = null;
-		this.score5Y  = null;
-		this.score10Y = null;
-	}
-	
+
 	@Override
 	public int compareTo(FundDivScore that) {
 		return this.isinCode.compareTo(that.isinCode);
 	}
-	
+
 	@Override
 	public String toString() {
 		return ToString.withFieldName(this);
